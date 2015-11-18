393274c179acd473d894cae104b6045f2099c868
Richard Martens <r.martens@smart4solutions.nl>
2015-11-18 09:31:55 +0100

    apex 4.2 compatibility
    I had forgotten to export the plugin as a 4.2 plugin.

---
+++ plugin_apex42/authentication_type_plugin_nl_s4s_oauth2.sql
@@ -0,0 +1,106 @@
+set define off
+set verify off
+set feedback off
+WHENEVER SQLERROR EXIT SQL.SQLCODE ROLLBACK
+begin wwv_flow.g_import_in_progress := true; end;
+/
+ 
+--       AAAA       PPPPP   EEEEEE  XX      XX
+--      AA  AA      PP  PP  EE       XX    XX
+--     AA    AA     PP  PP  EE        XX  XX
+--    AAAAAAAAAA    PPPPP   EEEE       XXXX
+--   AA        AA   PP      EE        XX  XX
+--  AA          AA  PP      EE       XX    XX
+--  AA          AA  PP      EEEEEE  XX      XX
+prompt  Set Credentials...
+ 
+begin
+ 
+  -- Assumes you are running the script connected to SQL*Plus as the Oracle user APEX_040200 or as the owner (parsing schema) of the application.
+  wwv_flow_api.set_security_group_id(p_security_group_id=>nvl(wwv_flow_application_install.get_workspace_id,63111607323168155));
+ 
+end;
+/
+
+begin wwv_flow.g_import_in_progress := true; end;
+/
+begin 
+
+select value into wwv_flow_api.g_nls_numeric_chars from nls_session_parameters where parameter='NLS_NUMERIC_CHARACTERS';
+
+end;
+
+/
+begin execute immediate 'alter session set nls_numeric_characters=''.,''';
+
+end;
+
+/
+begin wwv_flow.g_browser_language := 'en'; end;
+/
+prompt  Check Compatibility...
+ 
+begin
+ 
+-- This date identifies the minimum version required to import this file.
+wwv_flow_api.set_version(p_version_yyyy_mm_dd=>'2012.01.01');
+ 
+end;
+/
+
+prompt  Set Application ID...
+ 
+begin
+ 
+   -- SET APPLICATION ID
+   wwv_flow.g_flow_id := nvl(wwv_flow_application_install.get_application_id,103);
+   wwv_flow_api.g_id_offset := nvl(wwv_flow_application_install.get_offset,0);
+null;
+ 
+end;
+/
+
+prompt  ...ui types
+--
+ 
+begin
+ 
+null;
+ 
+end;
+/
+
+prompt  ...plugins
+--
+--application/shared_components/plugins/authentication_type/nl_s4s_oauth2
+ 
+begin
+ 
+wwv_flow_api.create_plugin (
+  p_id => 89822031284757322 + wwv_flow_api.g_id_offset
+ ,p_flow_id => wwv_flow.g_flow_id
+ ,p_plugin_type => 'AUTHENTICATION TYPE'
+ ,p_name => 'NL.S4S.OAUTH2'
+ ,p_display_name => 'S4S oAuth 2'
+ ,p_supported_ui_types => 'DESKTOP:JQM_SMARTPHONE'
+ ,p_session_sentry_function => '#OWNER#.s4sa_oauth_pck.auth_sentry'
+ ,p_authentication_function => '#OWNER#.s4sa_oauth_pck.authenticate'
+ ,p_substitute_attributes => true
+ ,p_subscribe_plugin_settings => true
+ ,p_version_identifier => '0.1'
+ ,p_about_url => 'https://github.com/smart4solutions/apex_oauth'
+  );
+null;
+ 
+end;
+/
+
+commit;
+begin
+execute immediate 'begin sys.dbms_session.set_nls( param => ''NLS_NUMERIC_CHARACTERS'', value => '''''''' || replace(wwv_flow_api.g_nls_numeric_chars,'''''''','''''''''''') || ''''''''); end;';
+end;
+/
+set verify on
+set feedback on
+set define on
+prompt  ...done
